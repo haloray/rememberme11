9ad0978e097fba7be9cafccfa2a6c5afee405413
haloray <30794078+haloray@users.noreply.github.com>
2019-04-19 09:09:05 +0800

    Add files via upload
    account、account-edit、account-familyTree、account-memorials搭出了ui框架
    还差edit中textfield文本反馈、照片上传以及美化工作

---
+++ rmm/rmm/CountingTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  CountingTabBarController.swift
+//  rmm
+//
+//  Created by se7en on 2019/4/11.
+//  Copyright © 2019 se7en. All rights reserved.
+//
+
+import UIKit
+
+class CountingTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
